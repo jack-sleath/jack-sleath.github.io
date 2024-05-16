f5fca2b4e85cb176b63f95240ad16ebb324ac150
jack-sleath <102583698+jack-sleath@users.noreply.github.com>
2024-05-16 08:46:26 +0100

    Update script.js

--- code/js/script.js
+++ code/js/script.js
@@ -20,7 +20,7 @@ function runCode() {
 }
 
 // Fetch and parse the JSON file
-fetch('../json/code.json')
+fetch('../code/json/code.json')
   .then(response => response.json())
   .then(data => {
     const dropdown = document.getElementById('example-dropdown');
